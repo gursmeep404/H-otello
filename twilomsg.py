750122e2c66fdccd08d94c9bb06a86e55a459e0f
prateekshukla17 <4944.prateek@gmail.com>
2025-06-30 17:08:37 +0530

    more changes

--- twilomsg.py
+++ twilomsg.py
@@ -3,19 +3,18 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-# Load from .env or config
+
 TWILIO_SID = os.getenv("TWILIO_SID")
 TWILIO_AUTH = os.getenv("TWILIO_AUTH")
 TWILIO_WHATSAPP_NUMBER = "whatsapp:+14155238886"  # Twilio sandbox number
 
 client = Client(TWILIO_SID, TWILIO_AUTH)
 
-# Get number from stored queries
-to_number = "whatsapp:+917607665801" # must be stored when query was received
+to_number = "whatsapp:+917607665801" 
 
 if to_number:
     message = client.messages.create(
-        body=f"✅ Here's the human-answered response: Hello how are you",
+        body=f"✅ Here's the response from the staff: Hello how are you",
         from_=TWILIO_WHATSAPP_NUMBER,
         to=to_number
     )
